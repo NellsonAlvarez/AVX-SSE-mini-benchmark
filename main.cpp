eb82b6dd2f190974e649c8d1513cdfc7b807773d
Nelson <nellsoncedeno@gmail.com>
2020-10-30 00:15:45 -0600

    Improved test method

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-// Nelson Alvarez
-// Dean Vargas
+// Authors:
+//          Nelson Alvarez
+//          Dean Vargas
 
 #include <chrono>
 #include <random>
 #include <iostream>
 #include <cstdint>
+#include <cstring>
 #include <climits>
 #include <functional>
 
@@ -22,38 +24,43 @@ typedef unsigned int u_int;
 typedef struct xmm_val xmm_val_t;
 
 
-float dot_prod_c(xmm_val_t*, xmm_val_t*);
+           float dot_prod_c(xmm_val_t*, xmm_val_t*);
 extern "C" float dot_prod_avx_p(xmm_val_t*, xmm_val_t*);
 extern "C" float dot_prod_avx_s(xmm_val_t*, xmm_val_t*);
 extern "C" float dot_prod_sse_p(xmm_val_t*, xmm_val_t*);
 extern "C" float dot_prod_sse_s(xmm_val_t*, xmm_val_t*);
 
 
-chrono::duration<double> do_test(xmm_val_t*, xmm_val_t*, int, mt19937&,function<float(xmm_val_t*, xmm_val_t*)>);
-void valid_test(xmm_val_t*, xmm_val_t*, int);
+void generate_values(xmm_val_t**, xmm_val_t**, int, mt19937&);
+void test_all(xmm_val_t**, xmm_val_t**, int, mt19937&);
+void validate(xmm_val_t*, xmm_val_t*, int);
+chrono::duration<double> do_test(xmm_val_t**, xmm_val_t**, int, function<float(xmm_val_t*, xmm_val_t*)>);
+bool isNumber(char* s);
 
 int main(int argc, char* argv[]){
-
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
     mt19937 generator(seed);
 
-    alignas(16) xmm_val_t vector1;
-    alignas(16) xmm_val_t vector2;
-
     u_int trials = 10000;
 
+    if(argc > 1){
+        if(isNumber(argv[1]))
+            trials = stoi(argv[1]);
+        else{
+            cout<<"Usage: "<<argv[0]<<" <number of tests>\n";
+            return 0;    
+        }
+    }
 
-    //cout<<"RESULT TEST\n";
+    alignas(16) xmm_val_t* vector1 = new xmm_val_t[trials];
+    alignas(16) xmm_val_t* vector2 = new xmm_val_t[trials];
 
-    //valid_test(&vector1,&vector2, 10);
+    test_all(&vector1,&vector2, trials, generator);
 
-    cout<<"TIME TEST\n";
+    delete vector1;
+    delete vector2;
 
-    cout<<"HIGH LEVEL TEST: "<<do_test(&vector1, &vector2, trials, generator, dot_prod_c).count()<<"s\n";
-    cout<<"AVX PACKED TEST: "<<do_test(&vector1, &vector2, trials, generator, dot_prod_avx_p).count()<<"s\n";
-    cout<<"AVX SCALAR TEST: "<<do_test(&vector1, &vector2, trials, generator, dot_prod_avx_s).count()<<"s\n";
-    cout<<"SSE PACKED TEST: "<<do_test(&vector1, &vector2, trials, generator, dot_prod_sse_p).count()<<"s\n";
-    cout<<"SSE SCALAR TEST: "<<do_test(&vector1, &vector2, trials, generator, dot_prod_sse_s).count()<<"s\n";
+    return 0;
 }
 
 float dot_prod_c(xmm_val_t* v1, xmm_val_t* v2){
@@ -63,36 +70,48 @@ float dot_prod_c(xmm_val_t* v1, xmm_val_t* v2){
     return result;
 }
 
-void valid_test(xmm_val_t *v1, xmm_val_t *v2, int n){
+void test_all(xmm_val_t** v1, xmm_val_t** v2, int n, mt19937& generator){
+    generate_values(v1,v2,n,generator);
+    cout<<"TIME TEST with "<<n<<" trials\n";
+
+    cout<<"HIGH LEVEL: "<<do_test(v1, v2, n, dot_prod_c).count()<<"s\n";
+    cout<<"AVX PACKED: "<<do_test(v1, v2, n, dot_prod_avx_p).count()<<"s\n";
+    cout<<"AVX SCALAR: "<<do_test(v1, v2, n, dot_prod_avx_s).count()<<"s\n";
+    cout<<"SSE PACKED: "<<do_test(v1, v2, n, dot_prod_sse_p).count()<<"s\n";
+    cout<<"SSE SCALAR: "<<do_test(v1, v2, n, dot_prod_sse_s).count()<<"s\n";
+}
+
+void validate(xmm_val_t *v1, xmm_val_t *v2, int n){
+    cout << "R1: "<<dot_prod_c(v1,v2)<<"\tR2: "<<dot_prod_avx_p(v1,v2)<<"\tR3: "<<dot_prod_avx_s(v1,v2)<<"\tR4: "<<dot_prod_sse_p(v1,v2)<<"\tR5: "<<dot_prod_sse_s(v1,v2)<<endl;
+}
+
+void generate_values(xmm_val_t** v1, xmm_val_t** v2, int n, mt19937& generator){
     for(u_int i = 0; i < n; ++i){
-        v1->m_f32[0] = 2.5;  
-        v1->m_f32[1] = 3.4;  
-        v1->m_f32[2] = 18.9; 
-        v1->m_f32[3] = 24.7; 
-        
-        v2->m_f32[0] = 120.4;
-        v2->m_f32[1] = 1.333;
-        v2->m_f32[2] = 6.1;  
-        v2->m_f32[3] = 3.2;  
-        cout << "R1: "<<dot_prod_c(v1,v2)<<"\tR2: "<<dot_prod_avx_p(v1,v2)<<"\tR3: "<<dot_prod_avx_s(v1,v2)<<"\tR4: "<<dot_prod_sse_p(v1,v2)<<"\tR5: "<<dot_prod_sse_s(v1,v2)<<endl;
+        (*v1+i)->m_f32[0] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
+        (*v1+i)->m_f32[1] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
+        (*v1+i)->m_f32[2] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
+        (*v1+i)->m_f32[3] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
+            
+        (*v2+i)->m_f32[0] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
+        (*v2+i)->m_f32[1] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
+        (*v2+i)->m_f32[2] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
+        (*v2+i)->m_f32[3] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
     }
 }
 
-chrono::duration<double> do_test(xmm_val_t* v1, xmm_val_t* v2, int n, mt19937& generator,function<float(xmm_val_t*, xmm_val_t*)> test){
+
+chrono::duration<double> do_test(xmm_val_t** v1, xmm_val_t** v2, int n, function<float(xmm_val_t*, xmm_val_t*)> test){
     chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
     for(u_int i = 0; i < n; ++i){
-        v1->m_f32[0] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
-        v1->m_f32[1] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
-        v1->m_f32[2] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
-        v1->m_f32[3] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
-        
-        v2->m_f32[0] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
-        v2->m_f32[1] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
-        v2->m_f32[2] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
-        v2->m_f32[3] = (float)(generator() % USHRT_MAX)/(float)USHRT_MAX;
-
-        test(v1,v2);
+        test((*v1+i),(*v2+i));
     }
     chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
     return chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+}
+
+bool isNumber(char* s){
+    for (int i = 0; i < strlen(s); ++i)
+        if (!isdigit(s[i]))
+            return false;
+    return true;
 }
